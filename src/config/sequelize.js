9b2ee6858e33eb565b20571b9632707cde9998d0
Jaeseong Seo <07js23@gmail.com>
2017-10-12 01:24:50 +0900

    Local dev db credentials

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 // database, username, password
-module.exports = new Sequelize('livle', 'livle', 'livledev', {
+module.exports = new Sequelize('livle', 'root', '12345', {
   host: "localhost",
   port: "3306",
   dialect: "mysql"

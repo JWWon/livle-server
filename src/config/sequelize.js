0742dcc4e9eb6f9ba75f04bd1102c72b1cf15afd
Jaeseong Seo <07js23@gmail.com>
2017-11-06 04:02:16 +0900

    DBName as ENV var

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -4,9 +4,12 @@ if(process.env.NODE_ENV === 'dev') {
   require('dotenv').config()
 }
 
-module.exports = new Sequelize('livle', process.env.DB_USER, process.env.DB_PASS, {
+module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: "mysql",
+  pool: {
+    max: 1
+  },
   operatorsAliases: false
 })

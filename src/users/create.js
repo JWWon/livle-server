cb5cd1e94df8c6b3ac408fffc918c6d2d8ad79e2
Jaeseong Seo <07js23@gmail.com>
2017-11-06 02:16:27 +0900

    Validations

--- src/users/create.js
+++ src/users/create.js
@@ -1,20 +1,27 @@
-'use strict';
+'use strict'
 const User = require('./user')
 const response = require('../response')
+
+const isValid = (email) => {
+  const regex = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  return regex.test(email)
+}
 
 module.exports = (event, context) => {
   const data = JSON.parse(event.body)
   if(!data.email || !data.password) {
     return context.succeed(response(400, null, "이메일과 비밀번호를 입력해주세요."))
   }
-  // TODO validate email
 
-  // TODO duplicate check
-  return User.create(data).then(user => {
-    const userData = user.dataValues
-    userData.token = user.getToken()
-    return context.succeed(response(200, userData))
-  }).catch(err => {
-    return context.fail(err)
+  if(!isValid(data.email))
+    return context.succeed(response(405, null, "이메일의 형식이 잘못 되었습니다."))
+
+  return User.create({ email: data.email, password: data.password })
+    .then((user) => {
+      var userData = user.dataValues
+      userData.token = user.getToken()
+      return context.succeed(response(200, userData))
+    }).catch(err => {
+      return context.succeed(response(403, null, "이미 가입되어 있는 이메일 주소입니다."))
   })
 }

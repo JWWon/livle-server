51e6045c00655298012f71e4ef31338e0dcf2a64
Jaeseong Seo <07js23@gmail.com>
2017-11-06 01:47:48 +0900

    AWS profile configuration, env var settings

--- src/config/sequelize.js
+++ src/config/sequelize.js
@@ -1,9 +1,12 @@
 const Sequelize = require('sequelize')
-// database, username, password
-module.exports = new Sequelize('livle', 'root', '12345', {
-  host: "localhost",
-  port: "3306",
+if(process.env.NODE_ENV === 'dev') {
+  // NODE_ENV 환경변수를 dev로 세팅할 경우, {root}/.env 파일에서 환경변수 읽어옵니다.
+  require('dotenv').config()
+}
+
+module.exports = new Sequelize('livle', process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: "mysql",
   operatorsAliases: false
 })
-

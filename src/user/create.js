a59fbc1e131d0f09ed87f8eb8a1000c7e7feefcd
Jaeseong Seo <07js23@gmail.com>
2017-11-06 14:59:08 +0900

    Use callback instead of context

--- src/user/create.js
+++ src/user/create.js
@@ -7,21 +7,23 @@ const isValid = (email) => {
   return regex.test(email)
 }
 
-module.exports = (event, context) => {
+module.exports = (event, context, callback) => {
+  //context.callbackWaitsForEmptyEventLoop = false;
+
   const data = JSON.parse(event.body)
   if(!data.email || !data.password) {
-    return context.succeed(response(400, null, "이메일과 비밀번호를 입력해주세요."))
+    return callback(null, response(400, null, "이메일과 비밀번호를 입력해주세요."))
   }
 
   if(!isValid(data.email))
-    return context.succeed(response(405, null, "이메일의 형식이 잘못 되었습니다."))
+    return callback(null, response(405, null, "이메일의 형식이 잘못 되었습니다."))
 
   return User.create({ email: data.email, password: data.password })
     .then((user) => {
       var userData = user.dataValues
       userData.token = user.getToken()
-      return context.succeed(response(200, userData))
+      return callback(null, response(200, userData))
     }).catch(err => {
-      return context.succeed(response(403, null, "이미 가입되어 있는 이메일 주소입니다."))
+      return callback(null, response(403, null, "이미 가입되어 있는 이메일 주소입니다."))
   })
 }

34607cb86711954d0199852d6953a60a613ec5ee
Jaeseong Seo <07js23@gmail.com>
2017-11-15 19:19:35 +0900

    Migration template

--- migrate.js
+++ migrate.js
@@ -1,5 +1,29 @@
-require('./src/user/user').sync()
-  .then(() => { console.log('User migrated'); process.exit() })
-  .catch(err => { console.error('Failed to migrate User', err); process.exit(1) })
+const userMigration = () => require('./src/user/user').sync()
+  .then(() => {
+    console.log('User migrated')
+    partnerMigration()
+  }).catch(err => {
+    console.error('Failed to migrate User', err)
+    process.exit(1)
+  })
+
+const partnerMigration = () => require('./src/partner/partner').sync()
+  .then(() => {
+    console.log('Parnter migrated')
+    ticketMigration()
+  }).catch(err => {
+    console.error('Failed to migrate Partner', err)
+    process.exit(1)
+  })
+
+const ticketMigration = () => require('./src/ticket/ticket').sync()
+  .then(() => {
+    console.log('Ticket migrated')
+    process.exit()
+  }).catch(err => {
+    console.error('Failed to migrate Ticket', err)
+    process.exit(1)
+  })
 
 
+userMigration()

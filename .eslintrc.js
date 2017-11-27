57f5c9cc9c410ca30810ba1a99315dbfb7763d80
Jaeseong Seo <07js23@gmail.com>
2017-11-28 00:50:08 +0900

    Linter added

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": "google",
+  "rules": {
+    "semi": [1, "never"],
+    "object-curly-spacing": ["error", "always"],
+    "keyword-spacing": ["error", { "before": true, "after": false }]
+  },
+  "parserOptions": {
+    "ecmaVersion": 6
+  }
+};

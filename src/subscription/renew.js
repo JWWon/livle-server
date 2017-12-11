cf209b49e629284620ff75142f1ddd9fa3d18a13
Jaeseong Seo <07js23@gmail.com>
2017-12-11 16:42:34 +0900

    Skeletion of renew function, and serverless config

---
+++ src/subscription/renew.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const iamport = require('../config/iamport')
+const Subscription = require('./subscription')
+const _ = require('lodash')
+const doPay = require('pay')
+
+module.exports = (params, respond) => {
+  const now = new Date()
+  Subscription.findAll().then((subscriptions) =>
+    _.map(subscriptions, s => {
+      // TODO implement
+    })
+  )
+}

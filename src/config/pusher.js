67e655a21344c947a5998cb1000f9705048a74d7
Jaeseong Seo <07js23@gmail.com>
2018-01-05 16:03:20 +0900

    Pusher added and configured

---
+++ src/config/pusher.js
@@ -0,0 +1,16 @@
+const Pusher = require('pusher');
+
+if (process.env.NODE_ENV === 'dev') {
+  // NODE_ENV 환경변수를 dev로 세팅할 경우, {root}/.env 파일에서 환경변수 읽어옵니다.
+  require('dotenv').config()
+}
+
+const pusher = new Pusher({
+  appId: '453625',
+  key: process.env.PUSHER_KEY,
+  secret: process.env.PUSHER_SECRET,
+  cluster: 'ap1',
+  encrypted: true
+});
+
+module.exports = pusher

cb73abf7e3aad82dc03fc0f66726758cd6d5d6ff
Jaeseong Seo <07js23@gmail.com>
2018-01-06 14:37:10 +0900

    Compile script of email templates

---
+++ templates/compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Replace @@header@@, @@footer@@ with the content of header.html, footer.html respectively
+HEADER=$(tr -d '\n' < header.html | sed -e 's/[\/&]/\\&/g')
+#HEADER=$(tr -d '\n' < header.html | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")
+FOOTER=$(tr -d '\n' < footer.html | sed -e 's/[\/&]/\\&/g')
+
+DEST="$PWD/dist"
+[ ! -d $DEST ] && mkdir -p $DEST || :
+
+shopt -s nullglob
+shopt -s extglob
+for f in !(header|footer).html
+do
+  sed "s/@@header@@/$HEADER/g; s/@@footer@@/$FOOTER/g" $f > "$DEST/$f"
+done

160374e1323a73205165a1e92c4096ba1bcf179f
Jaeseong Seo <07js23@gmail.com>
2018-01-06 15:18:03 +0900

    Signature refactored as a template

--- templates/compile.sh
+++ templates/compile.sh
@@ -4,6 +4,7 @@
 HEADER=$(tr -d '\n' < header.html | sed -e 's/[\/&]/\\&/g')
 #HEADER=$(tr -d '\n' < header.html | sed -e "s/'/'\\\\''/g; 1s/^/'/; \$s/\$/'/")
 FOOTER=$(tr -d '\n' < footer.html | sed -e 's/[\/&]/\\&/g')
+SIGNATURE=$(tr -d '\n' < signature.html | sed -e 's/[\/&]/\\&/g')
 
 DEST="$PWD/dist"
 [ ! -d $DEST ] && mkdir -p $DEST || :
@@ -12,5 +13,5 @@ shopt -s nullglob
 shopt -s extglob
 for f in !(header|footer).html
 do
-  sed "s/@@header@@/$HEADER/g; s/@@footer@@/$FOOTER/g" $f > "$DEST/$f"
+  sed "s/@@header@@/$HEADER/g; s/@@footer@@/$FOOTER/g; s/@@signature@@/$SIGNATURE/g" $f > "$DEST/$f"
 done

11804b6f0314ea32cd837fe687e627f8f8444afa
Jaeseong Seo <07js23@gmail.com>
2018-01-13 15:54:10 +0900

    Let userGet receive fcmToken

--- src/user/get.js
+++ src/user/get.js
@@ -4,10 +4,21 @@ const User = require('./user')
 
 module.exports = (params, respond) => {
   if (!params.auth) return respond(401, '로그인되지 않았습니다.')
+  const fcmToken = params.query && params.query.fcmToken
+  const updateFcmToken = (user) => new Promise((resolve, reject) => {
+      if (fcmToken && fcmToken !== user.fcm_token) {
+        user.update({ fcm_token: fcmToken })
+          .then((user) => resolve(user))
+          .catch((err) => reject(err))
+      } else {
+        resolve(user)
+      }
+  })
 
   return User.fromToken(params.auth)
-    .then((user) => user.deepUserData()
-      .then((userData) => respond(200, userData)
+    .then((user) =>
+      updateFcmToken(user).then((user) =>
+        user.deepUserData().then((userData) => respond(200, userData))
       ).catch((err) => respond(500, err))
     ).catch((err) => {
       console.error(err)

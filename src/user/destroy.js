090b63cdf5402cb14f10f726637a09bd413a49cd
Jaeseong Seo <07js23@gmail.com>
2018-01-13 19:20:41 +0900

    Handle null body

--- src/user/destroy.js
+++ src/user/destroy.js
@@ -5,10 +5,10 @@ const R = User.REJECTIONS
 module.exports = (params, respond) => {
   const body = params.body
   if (!body || !body.email || !body.password) {
-    respond(400, '이메일과 비밀번호를 입력해주세요.')
+    return respond(400, '이메일과 비밀번호를 입력해주세요.')
   }
 
-  return User.dropOut(params.body.email, params.body.password)
+  return User.dropOut(body.email, body.password)
     .then((success) => {
       if (success) {
         respond(200)
@@ -21,6 +21,7 @@ module.exports = (params, respond) => {
       } else if (err === R.WRONG_PASSWORD) {
         respond(403, '비밀번호가 틀립니다.')
       } else {
+        console.error(err)
         respond(500, err)
       }
     })

fa4251e99f422ce36bdd840ed09d679c6bde1628
Jaeseong Seo <07js23@gmail.com>
2018-01-13 15:25:51 +0900

    Add serverKey on deploy script and readme

--- src/send-push.js
+++ src/send-push.js
@@ -8,10 +8,12 @@ if (process.env.NODE_ENV === 'dev') {
 const serverKey = process.env.FCM_SERVER_KEY
 const fcm = new FCM(serverKey)
 
-module.exports = (to, body) =>
-  fcm.send({
+module.exports = (to, body) => {
+  if (!to) throw new Error('Missing device token')
+  return fcm.send({
     to: to,
     notification: {
       body: body,
     },
   })
+}
